52dfcc60db97199c2f43cf062b76207b67206428
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-18 11:39:18 +0200

    dailyRoutine: process video

--- dailyRoutine.py
+++ dailyRoutine.py
@@ -33,6 +33,11 @@ def main():
             nombre_video_salida = get_date_previous_day()
             ruta_carpeta_entrada_video = "./img/" + nombre_video_salida
             video.crear_video_desde_imagenes(ruta_carpeta_entrada_video, nombre_video_salida)
+            
+            # Video procesado
+            ruta_video_procesado_entrada = "./video/sin_procesar/" + nombre_video_salida
+            video.procesar_video(ruta_video_procesado_entrada, nombre_video_salida)
+
     except KeyboardInterrupt:
         print("\nCtrl + C detectado. Deteniendo el programa dailyRoutine.py.")
 
